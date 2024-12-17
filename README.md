701d069d8351d3f85aa32b9894988b6f2ac9baaf
lumiknit <aasr4r4@gmail.com>
2024-12-17 19:32:41 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Chopsticks Game Simulator
+
+Play chopsticks game with the computer.
+
+https://lumiknit.github.io/apps/chopsticks/
+
+## Build
+
+```bash
+pnpm install
+pnpm dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "@picocss/pico/css/pico.min.css";
+
+import { Toaster } from "solid-toast";
+
+function App() {
+  return (
+    <>
+      <Toaster />
+      <main class="container">
+        <h1>Hello, World!</h1>
+        <button>skd</button>
+      </main>
+    </>
+  );
+}
+
+export default App;
